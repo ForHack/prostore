7a4fbe1267c140fb434a681cfeceeaa41e7e7836
Odiljon <olimjonov-odiljon@mail.ru>
2025-05-24 17:34:18 +0500

    Section 3: 23. Load products From Database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import {getLatestProducts} from "@/lib/actions/product.actions";
 
-export default function Page() {
+export default async function Page() {
+    const latestProducts = await getLatestProducts()
+
     return (
-    <ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />
+    <ProductList data={latestProducts} title="Newest Arrivals" />
   );
 }
