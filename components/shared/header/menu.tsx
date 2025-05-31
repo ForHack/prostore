b3b5037538660cc11ec013b886415dd17c022218
Odiljon <olimjonov-odiljon@mail.ru>
2025-05-31 19:14:01 +0500

    Section 4: 39. User Button & Sign Out

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,8 +1,9 @@
 import ModeToggle from "@/components/shared/header/mode-toggle";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
-import {EllipsisVertical, ShoppingCartIcon, UserIcon} from "lucide-react";
+import {EllipsisVertical, ShoppingCartIcon} from "lucide-react";
 import {Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger} from "@/components/ui/sheet";
+import UserButton from "@/components/shared/header/user-button";
 
 const Menu = () => {
   return <div className="flex justify-end gap-3">
@@ -13,11 +14,8 @@ const Menu = () => {
                   <ShoppingCartIcon /> Cart
               </Link>
           </Button>
-          <Button asChild>
-              <Link href="/sign-in">
-                  <UserIcon /> Sign In
-              </Link>
-          </Button>
+
+          <UserButton />
       </nav>
 
       <nav className="md:hidden">
@@ -37,11 +35,7 @@ const Menu = () => {
                       </Link>
                   </Button>
 
-                  <Button asChild>
-                      <Link href="/sign-in">
-                          <UserIcon /> Sign In
-                      </Link>
-                  </Button>
+                 <UserButton />
 
                   <SheetDescription></SheetDescription>
               </SheetContent>
