e4a137d10f02ddb1b73913e5f20adce0bb219e35
Odiljon <olimjonov-odiljon@mail.ru>
2025-06-01 14:57:29 +0500

    temp commit

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,10 +1,33 @@
 'use server'
 
 import {CartItem} from "@/types";
+import {formatError} from "../utils";
+import {cookies} from "next/headers";
+import {auth} from "@/auth";
 
 export async function addItemToCart(data: CartItem) {
-    return {
-        success: true,
-        message: 'Item added to cart',
+    try {
+        // Check for cart cookie
+        const sessionCartId = (await cookies()).get('sessionCartId')?.value
+
+        if (!sessionCartId) {
+            throw new Error('Cart session not found.')
+        }
+
+        // Get session and user ID
+        const session = await auth()
+        const userId = session?.user?.id ? (session.user.id as string) : undefined
+
+        console.log('userId', userId)
+
+        return {
+            success: true,
+            message: 'Item added to cart',
+        }
+    } catch (error) {
+        return {
+            success: false,
+            message: formatError(error),
+        }
     }
 }
